7a6784fbff0975e19ecb1f05c91420b1996314db
Marshall Raiskin <marshall.raiskin@gmail.com>
2020-05-12 02:16:17 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,9 +1,9 @@
 # Write your methods here
-def loop_message_five_times(string)
-  message = "Hello World."
-  
+def loop_message_five_times(message)
+  count = 0
+  while count < 5 do
+    puts message
+    count += 1
+  end
 end
 
-def loop_message_n_times
-  
-end
