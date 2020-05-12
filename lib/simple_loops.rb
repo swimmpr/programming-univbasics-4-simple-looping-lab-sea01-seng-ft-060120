e6d766ab4b5d16a36cccd352df1ed4fd435fd97e
Marshall Raiskin <marshall.raiskin@gmail.com>
2020-05-12 02:29:23 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -26,7 +26,7 @@ end
 def return_string_array(array)
   count = 0
   while count < array.length do
-    new_array.<< array[count].to_s
+    new_array << array[count].to_s
     count += 1
   end
   new_array
